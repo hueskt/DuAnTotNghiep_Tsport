53022169270ff5b87f7073133a292721b9209476
yennobita <nguyenyen102003@gmail.com>
2023-10-07 19:06:35 +0700

    update all file

---
+++ T_Sport/routes/checkOut.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const checkOutController = require("../controller/CheckOutController");
+
+router.get("/", checkOutController.getIndex);
+router.post("/", checkOutController.postCheckOut);
+
+//
+
+module.exports = router;
---
+++ T_Sport/routes/index.js
@@ -0,0 +1,41 @@
+const productRouter = require("./product");
+const homeRouter = require("./home");
+const userRouter = require("./user");
+const checkOutRouter = require("./checkOut");
+const checkOutRouter2 = require("./checkOut2");
+
+const chatBotRouter = require("./chatbot");
+
+function route(app) {
+  app.use("/home", homeRouter);
+
+  app.use("/shop-grid", productRouter);
+
+  app.use("/", userRouter);
+
+  app.get("/shop-details", function (req, res) {
+    res.render("shop-details/shop-details");
+  });
+
+  app.get("/shoping-cart", function (req, res) {
+    res.render("shoping-cart/shoping-cart");
+  });
+
+  app.get("/blog", function (req, res) {
+    res.render("blog/blog");
+  });
+
+  app.get("/blog-details", function (req, res) {
+    res.render("blog-details/blog-details");
+  });
+
+  app.get("/contact", function (req, res) {
+    res.render("contact/contact");
+  });
+
+  app.use("/checkout", checkOutRouter);
+
+  //app.use("/webhook",chatBotRouter);
+}
+
+module.exports = route;
