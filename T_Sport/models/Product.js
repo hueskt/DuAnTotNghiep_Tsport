b61009072aec9191617eeab8812eab5bde62d838
hieucute0805 <diepminhhieuhieu0805@gmail.com>
2023-10-08 11:55:44 +0700

    done pages prod vs prod-detail

---
+++ T_Sport/models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Product = new Schema(
+  {
+    name: { type: String, maxlength: 255 },
+    details: { type: String, maxlength: 255 },
+    quanlity: { type: String },
+    price: { type: Number },
+    image: { type: String },
+
+    category: { type: String, maxlength: 255 },
+    idProduct: { type: String, maxlength: 255 },
+    listImgExtra: [],
+    listIdRating: [],
+    producer: { type: String, maxlength: 255 },
+  },
+  { collection: "product" }
+);
+
+module.exports = mongoose.model("Product", Product);
